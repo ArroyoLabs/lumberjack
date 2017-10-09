40f96703dad53545e4eb6647f63d6a6e8ffd5413
kkennethlee <kyung.kenneth.lee@gmail.com>
2017-10-09 09:55:09 -0700

    dynamically get events from the db

--- app/controllers/LogController.php
+++ app/controllers/LogController.php
@@ -12,12 +12,17 @@ class LogController extends \erdiko\controllers\Web
      */
     public function get($request, $response, $args)
     {
-        die("poptarts");
         $logService = new \app\models\LogService($this->container->em);
         $events = $logService->getEvents();
-        print "<pre>"; var_dump($events); die(); 
 
-        //set $events to $themeData['page']['logevents'];
+        $logEvents = array();
+        foreach ($events as $event){
+            $res['eventID'] = $event->getId();
+            $res['description'] = $event->getDescription();
+            $res['name'] = $event->getName();
+
+            $logEvents[] = $res;
+        }
 
         $view = 'layouts/log.html';
         $themeData['theme'] = \erdiko\theme\Config::get($this->container->get('settings')['theme']);
@@ -25,35 +30,34 @@ class LogController extends \erdiko\controllers\Web
         $themeData['page'] =  [
             'title' => "This is the Log Index Controller",
             'description' => "This is where all the log that were previously created",
-            'logevents' => [
-                [
-                    'title' => "Log 1",
-                    'eventID' => 1,
-                    'description' => "This is a filler content description for Log 1. These content will be dynamically generated in future.",
-                    'image_src' => "https://lorempixel.com/600/300/food/5/",
-                    'latest_update' => "Last update by aPerson x minutes ago",
-                    'href' => "/log/detail/1"
-                ],
-                [
-                    'title' => "Log 2",
-                    'eventID' => 2,
-                    'description' => "This is a filler content description for Log 2. These content will be dynamically generated in future.",
-                    'image_src' => "https://lorempixel.com/600/300/food/5/",
-                    'latest_update' => "Last update by aPerson x minutes ago",
-                    'href' => "/log/detail/2"
-                ],
-                [
-                    'title' => "Log 3",
-                    'eventID' => 3,
-                    'description' => "This is a filler content description for Log 3. These content will be dynamically generated in future.",
-                    'image_src' => "https://lorempixel.com/600/300/food/5/",
-                    'latest_update' => "Last update by aPerson x minutes ago",
-                    'href' => "/log/detail/3"
-                ]
-            ]
+            'logevents' => $logEvents
         ];
 
-
+        //
+        // [
+        //     'title' => "Log 1",
+        //     'eventID' => 1,
+        //     'description' => "This is a filler content description for Log 1. These content will be dynamically generated in future.",
+        //     'image_src' => "https://lorempixel.com/600/300/food/5/",
+        //     'latest_update' => "Last update by aPerson x minutes ago",
+        //     'href' => "/log/detail/1"
+        // ],
+        // [
+        //     'title' => "Log 2",
+        //     'eventID' => 2,
+        //     'description' => "This is a filler content description for Log 2. These content will be dynamically generated in future.",
+        //     'image_src' => "https://lorempixel.com/600/300/food/5/",
+        //     'latest_update' => "Last update by aPerson x minutes ago",
+        //     'href' => "/log/detail/2"
+        // ],
+        // [
+        //     'title' => "Log 3",
+        //     'eventID' => 3,
+        //     'description' => "This is a filler content description for Log 3. These content will be dynamically generated in future.",
+        //     'image_src' => "https://lorempixel.com/600/300/food/5/",
+        //     'latest_update' => "Last update by aPerson x minutes ago",
+        //     'href' => "/log/detail/3"
+        // ]
 
         return $this->container->theme->render($response, $view, $themeData);
     }
