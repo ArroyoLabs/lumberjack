307681ae1bb78893632b501767500629789d9bf7
kkennethlee <kyung.kenneth.lee@gmail.com>
2017-11-15 16:59:31 -0800

    Clean up EventController and LogController

--- app/controllers/EventController.php
+++ app/controllers/EventController.php
@@ -213,68 +213,11 @@ class EventController extends \erdiko\controllers\Web
             'event_name' => $eventDetails['event_name'],
             'description' => "Description of the log you just clicked yourself.",
             'entries' => $entries,
-            'createlog_href' => '/event/createlog/' . $eventID
+            'createlog_href' => '/log/create/' . $eventID
         
         ];
     
         return $this->container->theme->render($response, $view, $themeData);
     }
-
-    /**
-     * Depending on the type of event, generate the corresponding form. (String or Integer)
-     * 
-     */
-    public function getCreatelog($request, $response, $args)
-    {
-        $eventID = (int)$args['param'];
-
-        $eventService = new \app\models\EventService($this->container->em);
-        $eventDetails = $eventService->getEventDetail($eventID);
-        $eventTableName = $eventDetails['event']->getEventTableName();
-
-
-        if($eventTableName == "event_value_number") {
-
-            $form = (object) array (
-                "name" => "Numeric Log",
-                "placeholder" => "Numeric Value"
-            );
-
-        } else if($eventTableName == "event_value_string") {
-
-            $form = (object) array (
-                "name" => "Status Log",
-                "placeholder" => "Write the nature of the log here"
-            );
-
-        }
-
-        $view = 'layouts/createlog.html';
-
-        $themeData['theme'] = \erdiko\theme\Config::get($this->container->get('settings')['theme']);
-
-        $themeData['page'] = [
-            'event_id'         => $eventID,
-            'event_name'       => $eventDetails['event_name'],
-            'eventdetail_href' => '/event/detail/' . $eventID,
-            'event_table_name' => "event_value_number",
-            'form'             => $form
-        ];
-
-
-
-        return $this->container->theme->render($response, $view, $themeData);
-    }
-
-    public function postCreatelog($request, $response, $args)
-    {
-        var_dump($_POST); die('post');
-
-        //if number value, set values to event_value_number
-
-        //if string value, set values to event_value_string
-
-
-    }
-
+    
 }
--- app/controllers/LogController.php
+++ app/controllers/LogController.php
@@ -10,21 +10,62 @@ class LogController extends \erdiko\controllers\Web
     {
     }
 
-
-    function postAddInt($request, $response, $args)
+    /**
+     * Depending on the type of event, generate the corresponding form. (String or Integer)
+     * 
+     */
+    public function getCreate($request, $response, $args)
     {
-        $logService = new \app\models\LogService($this->container->em);
-        
-        $params = (Object) array_filter($_POST);
+        $eventID = (int)$args['param'];
 
-        $logService->addIntLog($params);
+        $eventService = new \app\models\EventService($this->container->em);
+        $eventDetails = $eventService->getEventDetail($eventID);
+        $eventTableName = $eventDetails['event']->getEventTableName();
 
-        
+
+        if($eventTableName == "event_value_number") {
+
+            $form = (object) array (
+                "name" => "Numeric Log",
+                "placeholder" => "Numeric Value"
+            );
+
+        } else if($eventTableName == "event_value_string") {
+
+            $form = (object) array (
+                "name" => "Status Log",
+                "placeholder" => "Write the nature of the log here"
+            );
+
+        }
+
+        $view = 'layouts/createlog.html';
+
+        $themeData['theme'] = \erdiko\theme\Config::get($this->container->get('settings')['theme']);
+
+        $themeData['page'] = [
+            'event_id'         => $eventID,
+            'event_name'       => $eventDetails['event_name'],
+            'eventdetail_href' => '/event/detail/' . $eventID,
+            'event_table_name' => "event_value_number",
+            'form'             => $form
+        ];
+
+
+
+        return $this->container->theme->render($response, $view, $themeData);
     }
 
-    function postAddStatus($request, $response, $args)
+    public function postCreate($request, $response, $args)
     {
-        var_dump($_POST); die('statusLog');
+        var_dump($_POST); die('post');
+
+        //if number value, set values to event_value_number
+
+        //if string value, set values to event_value_string
+
+
     }
+
 
 }
